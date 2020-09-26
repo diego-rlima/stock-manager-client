9dc90091590abe2d242d263c542b176b33b8feb4
Diego R. Lima <contact@diegoribeiro.net>
2020-09-26 18:42:50 -0300

    Creation of axios plugin and removed theme configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
@@ -27,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '~/plugins/axios'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -57,18 +56,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+      dark: false
     }
   },
 
---
+++ plugins/axios.js
@@ -0,0 +1,29 @@
+const flattenSuccessResponse = response => response.data.data || {}
+const flattenErrorResponse = error => Promise.reject(error.response.data || {})
+
+const showSuccessMessage = (response, store) => {
+  const message = response.data.message || null
+
+  if (message !== null) {
+    store.commit('messages/setSuccess', message)
+  }
+
+  return response
+}
+const showErrorMessage = (error, store) => {
+  const data = error.response.data
+  const message = data.message || 'The action could not be completed due to an error'
+  // const status = error.response.status || 500
+
+  store.commit('messages/setError', message)
+
+  return error
+}
+
+export default function ({ $axios, store }) {
+  $axios.interceptors.response.use(
+    response => showSuccessMessage(response, store),
+    error => showErrorMessage(error, store)
+  )
+  $axios.interceptors.response.use(flattenSuccessResponse, flattenErrorResponse)
+}
